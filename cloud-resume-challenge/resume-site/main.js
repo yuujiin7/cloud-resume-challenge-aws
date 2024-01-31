daf6fe65c2f44fa0154656eda1eccc94e8b0d781
yuujiin7 <74890010+yuujiin7@users.noreply.github.com>
2024-01-31 16:16:13 +0800

    Update main.js

--- cloud-resume-challenge/resume-site/main.js
+++ cloud-resume-challenge/resume-site/main.js
@@ -30,32 +30,19 @@ $(document).ready(function () {
     $(window).on('scroll', _.throttle(onScroll, 200));
 
     function visitorCount() {
-        // Create an AbortController instance
-        const controller = new AbortController();
-        const signal = controller.signal;
-    
-        // Set a timeout (in milliseconds), e.g., 5000 milliseconds (5 seconds)
-        const timeout = 1000;
-    
-        // Set the timeout for the fetch request
-        const timeoutId = setTimeout(() => {
-            controller.abort(); // Abort the request if it takes longer than the timeout
-        }, timeout);
-        
-        fetch('https://yc1mkn35qf.execute-api.us-east-1.amazonaws.com/Prod/put', { signal })
+        fetch('https://yc1mkn35qf.execute-api.us-east-1.amazonaws.com/Prod/put')
             .then(() => fetch('https://yc1mkn35qf.execute-api.us-east-1.amazonaws.com/Prod/get'))
             .then(response => response.json())
             .then((data) => {
-                clearTimeout(timeoutId); // Clear the timeout since the request completed within the timeout
                 document.getElementById('replaceme').innerText = data.visitor_count;
             })
             .catch(error => {
-                clearTimeout(timeoutId); // Clear the timeout in case of an error
                 console.error('Error fetching visitor count:', error);
             });
     }
     
     visitorCount();
+    
     
     
 });
